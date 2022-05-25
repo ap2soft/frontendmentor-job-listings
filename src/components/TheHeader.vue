0081a785250af41a4fd4249700d9df442e56b3e0
Andrew Ponomarev <spargo32@gmail.com>
2022-05-25 08:54:58 +0300

    Fix Header background

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -6,6 +6,6 @@ export default defineComponent({});
 
 <template>
   <header
-    class="h-48 bg-cyan-dark bg-header-mobile bg-cover bg-center bg-no-repeat md:bg-header-desktop"
+    class="h-48 bg-cyan-dark bg-header-mobile bg-cover bg-center bg-no-repeat lg:bg-header-desktop"
   ></header>
 </template>
